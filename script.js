4448f00df35abc63a2fde4aec68ed4c2935af6bc
manishtomar998 <99527089+manishtomar998@users.noreply.github.com>
2023-01-17 15:45:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
-
+	let rev="";
+	for(let i=str.length-1;i>=0;i--){
+		rev+=str[i];
+	}
+	let revF=rev.toLowerCase();
+	let strF=str.toLowerCase();
+	if(revF==strF){
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 module.exports = palindrome
